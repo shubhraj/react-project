97749743f062348834f707319791e90c8e343e37
Shubham patil <shubham.patil@sincrodigital.com>
2024-07-05 18:42:33 +0530

    adding stop watch

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Body from './components/Body';
 import './App.css';
 import Accordion from "./components/Accordion";
+import Stopwatch from "./components/Stopwatch";
 
 function App() {
 
@@ -13,6 +14,10 @@ function App() {
     {
       path: "/accordion",
       element: <Accordion/>
+    },
+    {
+      path: "/stopwatch",
+      element: <Stopwatch/>
     }
   ]);
 
@@ -23,6 +28,7 @@ function App() {
         <nav className='flex justify-between p-2 mx-2 text-lg gap-5 flex-wrap ' >
           <a href='/' className='nav-link'> Meme Generator</a>
           <a href='/accordion' className='nav-link'> Accordion</a>
+          <a href='/stopwatch' className='nav-link'> Stopwatch</a>
         </nav>   
       </header>
     
